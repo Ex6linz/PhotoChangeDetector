ebdd0f455d3b3bf888b32f9d3a2b7bc9a7b5e54e
Artur <73757154+Ex6linz@users.noreply.github.com>
2024-10-03 21:58:49 +0200

    Delete PhotoChangeDetector/app directory

--- PhotoChangeDetector/app/templates/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('file_upload').addEventListener('change', function () {
-  const fileName = this.value.split('\\').pop();
-  document.getElementById('file_name').textContent = fileName ? `Selected file: ${fileName}` : '';
-});
